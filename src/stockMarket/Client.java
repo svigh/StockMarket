3970be652dda93c1f988863fe8e5d1afa163dee5
sebastian <vigh.sebastian@gmail.com>
2019-11-24 19:32:32 +0200

    Added more options for clients to do

--- src/stockMarket/Client.java
+++ src/stockMarket/Client.java
@@ -19,46 +19,97 @@ public class Client implements Runnable {
     @Override
     public void run() {
         int selection = 0;
-        System.out.println("Waiting for client input. Possible actions:\n" +
-                "1. Create offer\n" +
-                "2. Delete offer\n" +
-                "3. Modify offer\n");
-
+        String type;
+        String stock;
+        int stockCount, unitPrice, index;
         while( true ){
+
+            System.out.println("Waiting for client input. Possible actions:\n" +
+                    "1. Create offer\n" +
+                    "2. Delete offer\n" +
+                    "3. Modify offer\n" +
+                    "4. Print Offers on StockMarket\n" +
+                    "5. Get stock quote\n");
             Scanner input = new Scanner(System.in);
             selection = input.nextInt();
-            if ( selection == 0 )
-                break;
-            if ( selection == 1 ) {
-                System.out.println("\tYou need to create an offer\n");
-                System.out.println("Type:");
-                String type = input.next();
-                System.out.println("Stock:");
-                String stock = input.next();
-                Stock tempStock = new Stock(stock, this);
-                System.out.println("Stock count:");
-                int stockCount = input.nextInt();
-                System.out.println("Unit price:");
-                int unitPrice = input.nextInt();
-                Offer tempOffer = new Offer(0, type, tempStock, stockCount, unitPrice, this);
-                createOffer(tempOffer);
-                System.out.println("Created offer " + tempOffer.toString());
-            }
-            if ( selection == 2 ) {
-                System.out.println("\tGive the offer index to delete\n");
-                int index = input.nextInt();
-                deleteOffer(index);
-            }
-            if ( selection == 3 ) {
-                System.out.println("\tWhich offer to modify and to what\n");
-                int index = input.nextInt();
-                System.out.println("Stock count:");
-                int stockCount = input.nextInt();
-                System.out.println("Unit price:");
-                int unitPrice = input.nextInt();
-                System.out.println("Stock:");
-                String stock = input.next();
-                modifyOffer(index, stockCount, unitPrice, stock);
+            switch ( selection ) {
+                case 1:
+                    System.out.println("You need to create an offer\n");
+
+                    System.out.println("Type:");
+                    type = input.next();
+
+                    System.out.println("Stock:");
+                    stock = input.next();
+
+                    Stock tempStock = new Stock(stock, this);
+                    System.out.println("Stock count:");
+
+                    stockCount = input.nextInt();
+                    System.out.println("Unit price:");
+
+                    unitPrice = input.nextInt();
+                    Offer tempOffer = new Offer(0, type, tempStock, stockCount, unitPrice, this);
+
+                    createOffer(tempOffer);
+                    System.out.println("Created offer " + tempOffer.toString());
+                    break;
+                case 2:
+                    System.out.println("Give the offer index to delete\n");
+                    index = input.nextInt();
+
+                    deleteOffer(index);
+                    break;
+                case 3:
+                    System.out.println("Which offer to modify and to what\n");
+                    index = input.nextInt();
+
+                    System.out.println("Stock count:");
+                    stockCount = input.nextInt();
+
+                    System.out.println("Unit price:");
+                    unitPrice = input.nextInt();
+
+                    System.out.println("Stock:");
+                    stock = input.next();
+
+                    modifyOffer(index, stockCount, unitPrice, stock);
+                    break;
+                case 4:
+                    System.out.println("1. Sells\n2. Buys\n");
+                    index = input.nextInt();
+                    switch (index) {
+                        case 1:
+                            stockMarket.printPendingSell();
+                            break;
+                        case 2:
+                            stockMarket.printPendingBuy();
+                            break;
+                        default:
+                            System.out.println("Option doesn't exist\n");
+                            break;
+                    }
+                    break;
+                case 5:
+                    System.out.println("Company name:");
+                    stock = input.next();
+                    System.out.println("1. Sells\n2. Buys\n");
+                    index = input.nextInt();
+                    switch (index) {
+                        case 1:
+                            System.out.println(stock + " " + stockMarket.getSellPrice(stock));
+                            break;
+                        case 2:
+                            System.out.println(stock + " " +  stockMarket.getBuyPrice(stock));
+                            break;
+                        default:
+                            System.out.println("Option doesn't exist\n");
+                            break;
+                    }
+                    break;
+                default:
+                    System.out.println("Option doesn't exist\n");
+                    break;
             }
         }
     }
--- src/stockMarket/Main.java
+++ src/stockMarket/Main.java
@@ -10,17 +10,17 @@ public class Main {
         // Actionable elements
         StockMarket SM = new StockMarket(StocksArray);
         Client cl1 = new Client("Bob", SM);
-        Client cl2 = new Client("Jon", SM);
+//        Client cl2 = new Client("Jon", SM);
 
         // Each client is a thread
         Thread cl1_thread = new Thread(cl1);
         cl1_thread.start();
-        Thread cl2_thread = new Thread(cl2);
-        cl2_thread.start();
+//        Thread cl2_thread = new Thread(cl2);
+//        cl2_thread.start();
 
         // Add some Stocks in the names of each client
         Stock appl = new Stock("Apple", cl1);
-        Stock msft = new Stock("Microsoft", cl2);
+        Stock msft = new Stock("Microsoft", cl1);
 
         for (int i = 0; i < 10; ++i) StocksArray.add(appl);
         for (int i = 0; i < 10; ++i) StocksArray.add(msft);
@@ -28,7 +28,7 @@ public class Main {
 
 //********TESTING LOGIC****************************************
         // Create the test offers for client 1
-        Offer buy1 = new Offer(0, "buy", appl, 3, 5, cl1);
+//        Offer buy1 = new Offer(0, "buy", appl, 3, 5, cl1);
 //        Offer buy2 = new Offer(0, "buy", appl, 3, 5, cl1);
 //        Offer buy3 = new Offer(0, "buy", msft, 5, 10, cl1);
 //        Offer sell2 = new Offer(0, "sell", appl, 10, 8, cl1);
@@ -37,13 +37,13 @@ public class Main {
 //        Offer sell5 = new Offer(0, "sell", msft, 10, 3, cl1);
 
         // Create te test offers for client 2
-        Offer sell1 = new Offer(0, "sell", appl, 7, 3, cl2);
+//        Offer sell1 = new Offer(0, "sell", appl, 7, 3, cl2);
 //        Offer buy2 = new Offer(0, "buy", appl, 10, 3, cl2);
 //        Offer buy3 = new Offer(0, "buy", msft, 10, 2, cl2);
 //        Offer buy4 = new Offer(0, "buy", msft, 10, 6, cl2);
 
         // Client 1 registers some offers
-        cl1.createOffer(buy1);
+//        cl1.createOffer(buy1);
 //        cl1.createOffer(buy2);
 //        cl1.createOffer(buy3);
 //        cl1.createOffer(sell2);
@@ -52,27 +52,27 @@ public class Main {
 //        cl1.createOffer(sell5);
 
         // Client 2 registers some offers
-        cl2.createOffer(sell1);
+//        cl2.createOffer(sell1);
 //        cl2.createOffer(buy2);
 //        cl2.createOffer(buy3);
 //        cl2.createOffer(buy4);
 
 //******PRINTING THE INNER INFO****************
-        System.out.println("SELLS\n");
-        SM.printPendingSell();
+//        System.out.println("SELLS\n");
+//        SM.printPendingSell();
+//
+//        System.out.println("BUYS\n");
+//        SM.printPendingBuy();
+//
+//        System.out.println("TRANSACTIONS:\n");
+//        SM.printTransactionsHistory();
 
-        System.out.println("BUYS\n");
-        SM.printPendingBuy();
-
-        System.out.println("TRANSACTIONS:\n");
-        SM.printTransactionsHistory();
-
-        System.out.println("STOCKS:\n");
-        SM.printStocks();
+//        System.out.println("STOCKS:\n");
+//        SM.printStocks();
 
         // Ask the StockMarket for some index price
-        System.out.println("Buy price of Apple "  + SM.getBuyPrice("Apple"));
-        System.out.println("Sell price of Apple " + SM.getSellPrice("Apple"));
+//        System.out.println("Buy price of Apple "  + SM.getBuyPrice("Apple"));
+//        System.out.println("Sell price of Apple " + SM.getSellPrice("Apple"));
     }
 }
 
